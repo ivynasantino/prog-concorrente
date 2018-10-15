d75600fef4e895ec3f6b29f6e13163af1f02f7b7
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2018-10-15 15:07:46 -0300

    adiciona implementação do channel Co-authored-by: Valter valter.lucena@ccc.ufcg.edu.br

--- lista_1/primeira_questao/src/channel/ChannelImpl.java
+++ lista_1/primeira_questao/src/channel/ChannelImpl.java
@@ -1,17 +1,57 @@
 package channel;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.naming.LimitExceededException;
+import javax.naming.SizeLimitExceededException;
+
 public class ChannelImpl implements Channel {
 
+	private Queue<String> channel;
+	private int limit;
+	
+	public ChannelImpl(int limit) {
+		this.channel = new LinkedList<>();
+		this.limit = limit;
+	}
+
 	@Override
-	public void putMessage(String message) {
-		// TODO Auto-generated method stub
+	public void putMessage(String message){
+		if (!isFull()) {
+			this.channel.add(message);
+		} else {
+			throw new UnsupportedOperationException("The channel is full.");
+		}
 
 	}
 
 	@Override
 	public String takeMessage() {
-		// TODO Auto-generated method stub
-		return null;
+		String message = this.channel.peek();
+		
+		if (!isEmpty()) {
+			this.channel.remove();
+		} else {
+			throw new UnsupportedOperationException("The channel is empty.");
+		}
+		
+		return message;
+		
 	}
+	
+	private boolean isFull() {
+		return this.getLimit() == this.channel.size();
+	}
+	
+	private boolean isEmpty() {
+		return this.channel.isEmpty();
+	}
+	
+	public int getLimit() {
+		return this.limit;
+	}
+	
+	
 
 }
--- lista_1/primeira_questao/src/process/Sender.java
+++ lista_1/primeira_questao/src/process/Sender.java
@@ -1,11 +1,19 @@
 package process;
 
+import channel.Channel;
+
 public class Sender implements Runnable {
+
+	private Channel channel;
+	
+	public Sender(Channel channel) {
+		this.channel = channel;
+	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 }
