ccc6003296066d812b411dece3cc5c0a8d82747f
Valter Lucena <valter.lucena@ccc.ufcg.edu.br>
2018-12-08 20:08:55 -0300

    modifica questao01

--- lista_2/questao01.go
+++ lista_2/questao01.go
@@ -8,21 +8,21 @@ var mirrors = [3]string{"mirror1.com", "mirror2.br", "mirror3.edu"}
 var done = make(chan bool)
 var primeiro = ""
 
-func reliableRequest() {	
+func reliableRequest() string {	
 	go request(mirrors[0])
 	go request(mirrors[1])
 	go request(mirrors[2])
+	<- done
+	return primeiro
 }
 
 func request(serverName string) {
 	if (primeiro == "") {
 		primeiro = serverName
 	}
-	fmt.Println("o primeiro a responder foi ", primeiro)
 	done <- true
 }
 
 func main() {
-	reliableRequest()
-	<- done
+	fmt.Println("o primeiro a responder foi ", reliableRequest())
 }
